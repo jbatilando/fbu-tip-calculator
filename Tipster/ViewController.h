fa5733f5089f0e39c8f61c6931fc6053da3d127b
Miguel Batilando <miguel.batilando@gmail.com>
2019-05-19 10:59:16 -0700

    Initial Commit

---
+++ Tipster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tipster
+//
+//  Created by Miguel Batilando on 5/19/19.
+//  Copyright © 2019 Miguel Batilando. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
